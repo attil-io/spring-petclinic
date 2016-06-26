2272e00dbdbafdc0c61a7897e56355289da60aba
Attilio <parrotchaffinch@gmail.com>
2016-06-26 08:49:43 +0200

    VisitControllerTests uses mocks.

--- src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
@@ -1,18 +1,7 @@
 package org.springframework.samples.petclinic.web;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,6 +9,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.Visit;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 /**
  * Test class for {@link VisitController}
@@ -32,16 +43,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("spring-data-jpa")
 public class VisitControllerTests {
 
-    private static final int TEST_PET_ID = 1;
+    private static final String TEST_VISIT_DATE = "2013-01-01";
+	private static final int TEST_PET_ID = 1;
+    private static final int TEST_VISIT_ID = 100;
 
-    @Autowired
+    @InjectMocks
     private VisitController visitController;
+
+    @Mock
+    private ClinicService clinicService;
 
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
+
         this.mockMvc = MockMvcBuilders.standaloneSetup(visitController).build();
+        
+        
+        when(clinicService.findPetById(anyInt())).thenReturn(new Pet());
+        Collection<Visit> visits = new ArrayList<>();
+        visits.add(new Visit(){{ 
+            setDate(new LocalDate(TEST_VISIT_DATE));
+            setId(TEST_VISIT_ID);}});
+        when(clinicService.findVisitsByPetId(anyInt())).thenReturn(visits);
     }
 
     @Test
@@ -84,7 +110,7 @@ public class VisitControllerTests {
         ResultActions actions = mockMvc.perform(get("/owners/*/pets/{petId}/visits.json", 7).accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
         actions.andExpect(content().contentType("application/json;charset=UTF-8"))
-            .andExpect(jsonPath("$.visitList[0].id").value(1));
+            .andExpect(jsonPath("$.visitList[0].id").value(TEST_VISIT_ID));
     }
 
     @Test
@@ -92,6 +118,6 @@ public class VisitControllerTests {
         ResultActions actions = mockMvc.perform(get("/owners/*/pets/{petId}/visits.json", 7).accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
         actions.andExpect(content().contentType("application/json;charset=UTF-8"))
-            .andExpect(jsonPath("$.visitList[0].date").value("2013-01-01"));
+            .andExpect(jsonPath("$.visitList[0].date").value(TEST_VISIT_DATE));
     }
 }
