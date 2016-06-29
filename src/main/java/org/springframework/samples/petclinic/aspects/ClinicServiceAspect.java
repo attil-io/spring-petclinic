03f9964a813a46bf578295e5cccac58631934ded
Attilio <parrotchaffinch@gmail.com>
2016-06-29 06:16:31 +0200

    Log arguments and return values as well.

--- src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
@@ -1,8 +1,8 @@
 package org.springframework.samples.petclinic.aspects;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +10,17 @@ import org.slf4j.LoggerFactory;
 public class ClinicServiceAspect {
     final static Logger logger = LoggerFactory.getLogger(ClinicServiceAspect.class);
     
-    @Before("target(org.springframework.samples.petclinic.service.ClinicService)")
-    public void beforeServiceCall(JoinPoint jp) {
-        logger.info("Clinic Service Method called: " + jp.getSignature().getDeclaringType().getName() + "." + jp.getSignature().getName());
+    @Around("target(org.springframework.samples.petclinic.service.ClinicService)")
+    public void beforeServiceCall(ProceedingJoinPoint jp) throws Throwable {
+        logger.info("--> Clinic Service: " 
+                + jp.getSignature().getDeclaringType().getName() 
+                + "." + jp.getSignature().getName()
+                + "(" + jp.getArgs() + ")");
+        try {
+            Object ret = jp.proceed();
+            logger.info("<-- returned: " + ret);
+        } catch (Exception e) {
+            logger.info("<-- exception: " + e, e);
+        }
     }
 }
