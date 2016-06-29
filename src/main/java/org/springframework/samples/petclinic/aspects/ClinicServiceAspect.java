c8c38b92cc63e9e359d9065f647e64d8743682b9
Attilio <parrotchaffinch@gmail.com>
2016-06-29 05:59:12 +0200

    Log calls to clinic service into audit.log

--- src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
@@ -3,12 +3,15 @@ package org.springframework.samples.petclinic.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Aspect
 public class ClinicServiceAspect {
-
+    final static Logger logger = LoggerFactory.getLogger(ClinicServiceAspect.class);
+    
     @Before("target(org.springframework.samples.petclinic.service.ClinicService)")
     public void beforeServiceCall(JoinPoint jp) {
-        System.err.println("Clinic Service Method called: " + jp.getSignature().getDeclaringType().getName() + "." + jp.getSignature().getName());
+        logger.info("Clinic Service Method called: " + jp.getSignature().getDeclaringType().getName() + "." + jp.getSignature().getName());
     }
 }
