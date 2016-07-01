150ddb48e65ab0023bd5b18566bcc4c295ba408d
Attilio <parrotchaffinch@gmail.com>
2016-07-01 06:22:40 +0200

    Aspect returns the result of the service call.

--- src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
@@ -11,16 +11,18 @@ public class ClinicServiceAspect {
     final static Logger logger = LoggerFactory.getLogger(ClinicServiceAspect.class);
     
     @Around("target(org.springframework.samples.petclinic.service.ClinicService)")
-    public void beforeServiceCall(ProceedingJoinPoint jp) throws Throwable {
+    public Object beforeServiceCall(ProceedingJoinPoint jp) throws Throwable {
+        Object ret = null;
         logger.info("--> Clinic Service: " 
                 + jp.getSignature().getDeclaringType().getName() 
                 + "." + jp.getSignature().getName()
                 + "(" + jp.getArgs() + ")");
         try {
-            Object ret = jp.proceed();
+            ret = jp.proceed();
             logger.info("<-- returned: " + ret);
         } catch (Exception e) {
             logger.info("<-- exception: " + e, e);
         }
+        return ret;
     }
 }
