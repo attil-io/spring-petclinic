04ffe9e0708cc5667f54799e93cd83cd18864214
Attilio <parrotchaffinch@gmail.com>
2016-07-01 21:12:55 +0200

    Arguments are written out, compilation target is Java 8.

--- src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aspects/ClinicServiceAspect.java
@@ -1,5 +1,8 @@
 package org.springframework.samples.petclinic.aspects;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,12 +14,12 @@ public class ClinicServiceAspect {
     final static Logger logger = LoggerFactory.getLogger(ClinicServiceAspect.class);
     
     @Around("target(org.springframework.samples.petclinic.service.ClinicService)")
-    public Object beforeServiceCall(ProceedingJoinPoint jp) throws Throwable {
+    public Object audit(ProceedingJoinPoint jp) throws Throwable {
         Object ret = null;
         logger.info("--> Clinic Service: " 
                 + jp.getSignature().getDeclaringType().getName() 
                 + "." + jp.getSignature().getName()
-                + "(" + jp.getArgs() + ")");
+                + "(" + Arrays.stream(jp.getArgs()).map(Object::toString).collect(Collectors.joining(", ")) + ")");
         try {
             ret = jp.proceed();
             logger.info("<-- returned: " + ret);
