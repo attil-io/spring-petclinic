f48711382b6c145621a99f94a0316dfbf5156e47
Attilio <parrotchaffinch@gmail.com>
2016-07-17 15:45:03 +0200

    Minimal version, with only save method for petRepository.

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -1,113 +1,46 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-import org.springframework.beans.support.MutableSortDefinition;
-import org.springframework.beans.support.PropertyComparator;
-import org.springframework.format.annotation.DateTimeFormat;
-
-/**
- * Simple business object representing a pet.
- *
- * @author Ken Krebs
- * @author Juergen Hoeller
- * @author Sam Brannen
- */
 @Entity
 @Table(name = "pets")
-public class Pet extends NamedEntity {
+public class Pet {
 
-    @Column(name = "birth_date")
-    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
-    @DateTimeFormat(pattern = "yyyy/MM/dd")
-    private LocalDate birthDate;
+    @Column(name = "name")
+    private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "type_id")
-    private PetType type;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
-    private Owner owner;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
-    private Set<Visit> visits;
-
-    public LocalDate getBirthDate() {
-        return this.birthDate;
+    public Integer getId() {
+        return id;
     }
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public PetType getType() {
-        return this.type;
+    public boolean isNew() {
+        return this.id == null;
+    }    
+    
+    public String getName() {
+        return this.name;
     }
 
-    public void setType(PetType type) {
-        this.type = type;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Owner getOwner() {
-        return this.owner;
-    }
-
-    protected void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    protected Set<Visit> getVisitsInternal() {
-        if (this.visits == null) {
-            this.visits = new HashSet<>();
-        }
-        return this.visits;
-    }
-
-    protected void setVisitsInternal(Set<Visit> visits) {
-        this.visits = visits;
-    }
-
-    public List<Visit> getVisits() {
-        List<Visit> sortedVisits = new ArrayList<>(getVisitsInternal());
-        PropertyComparator.sort(sortedVisits, new MutableSortDefinition("date", false, false));
-        return Collections.unmodifiableList(sortedVisits);
-    }
-
-    public void addVisit(Visit visit) {
-        getVisitsInternal().add(visit);
-        visit.setPet(this);
+    @Override
+    public String toString() {
+        return this.getName();
     }
 
 }
--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPetRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPetRepositoryImpl.java
@@ -1,20 +1,6 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.repository.jdbc;
 
+import java.security.acl.Owner;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,24 +15,10 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.orm.ObjectRetrievalFailureException;
-import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.model.Visit;
-import org.springframework.samples.petclinic.repository.OwnerRepository;
 import org.springframework.samples.petclinic.repository.PetRepository;
-import org.springframework.samples.petclinic.repository.VisitRepository;
-import org.springframework.samples.petclinic.util.EntityUtils;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author Ken Krebs
- * @author Juergen Hoeller
- * @author Rob Harrop
- * @author Sam Brannen
- * @author Thomas Risberg
- * @author Mark Fisher
- */
 @Repository
 public class JdbcPetRepositoryImpl implements PetRepository {
 
@@ -54,54 +26,13 @@ public class JdbcPetRepositoryImpl implements PetRepository {
 
     private SimpleJdbcInsert insertPet;
 
-    private OwnerRepository ownerRepository;
-
-    private VisitRepository visitRepository;
-
-
     @Autowired
-    public JdbcPetRepositoryImpl(DataSource dataSource, OwnerRepository ownerRepository, VisitRepository visitRepository) {
+    public JdbcPetRepositoryImpl(DataSource dataSource) {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 
         this.insertPet = new SimpleJdbcInsert(dataSource)
             .withTableName("pets")
             .usingGeneratedKeyColumns("id");
-
-        this.ownerRepository = ownerRepository;
-        this.visitRepository = visitRepository;
-    }
-
-    @Override
-    public List<PetType> findPetTypes() throws DataAccessException {
-        Map<String, Object> params = new HashMap<>();
-        return this.namedParameterJdbcTemplate.query(
-            "SELECT id, name FROM types ORDER BY name",
-            params,
-            BeanPropertyRowMapper.newInstance(PetType.class));
-    }
-
-    @Override
-    public Pet findById(int id) throws DataAccessException {
-        JdbcPet pet;
-        try {
-            Map<String, Object> params = new HashMap<>();
-            params.put("id", id);
-            pet = this.namedParameterJdbcTemplate.queryForObject(
-                "SELECT id, name, birth_date, type_id, owner_id FROM pets WHERE id=:id",
-                params,
-                new JdbcPetRowMapper());
-        } catch (EmptyResultDataAccessException ex) {
-            throw new ObjectRetrievalFailureException(Pet.class, id);
-        }
-        Owner owner = this.ownerRepository.findById(pet.getOwnerId());
-        owner.addPet(pet);
-        pet.setType(EntityUtils.getById(findPetTypes(), PetType.class, pet.getTypeId()));
-
-        List<Visit> visits = this.visitRepository.findByPetId(pet.getId());
-        for (Visit visit : visits) {
-            pet.addVisit(visit);
-        }
-        return pet;
     }
 
     @Override
@@ -112,8 +43,8 @@ public class JdbcPetRepositoryImpl implements PetRepository {
             pet.setId(newKey.intValue());
         } else {
             this.namedParameterJdbcTemplate.update(
-                "UPDATE pets SET name=:name, birth_date=:birth_date, type_id=:type_id, " +
-                    "owner_id=:owner_id WHERE id=:id",
+                "UPDATE pets SET name=:name, " +
+                    " WHERE id=:id",
                 createPetParameterSource(pet));
         }
     }
@@ -125,9 +56,7 @@ public class JdbcPetRepositoryImpl implements PetRepository {
         return new MapSqlParameterSource()
             .addValue("id", pet.getId())
             .addValue("name", pet.getName())
-            .addValue("birth_date", pet.getBirthDate().toDate())
-            .addValue("type_id", pet.getType().getId())
-            .addValue("owner_id", pet.getOwner().getId());
+;
     }
 
 }
