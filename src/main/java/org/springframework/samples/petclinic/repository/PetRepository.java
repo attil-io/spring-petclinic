8b476517103b5810db48b29b1f33d61163b54f93
Attilio <parrotchaffinch@gmail.com>
2016-07-17 16:53:26 +0200

    Removed unnecessary implementations/tests.

--- src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
@@ -1,8 +1,10 @@
 package org.springframework.samples.petclinic.repository;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.samples.petclinic.model.Pet;
 
+@NoRepositoryBean
 public interface PetRepository {
 
     /**
