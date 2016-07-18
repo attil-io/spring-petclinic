0f39461e5378ce148a714a0e4d9310590194aad3
Attilio <parrotchaffinch@gmail.com>
2016-07-18 21:13:17 +0200

    Redirection to the right place, after adding vet.

--- src/main/java/org/springframework/samples/petclinic/web/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetController.java
@@ -82,7 +82,7 @@ public class VetController {
             return VIEWS_VET_CREATE_OR_UPDATE_FORM;
         } else {
             this.clinicService.saveVet(vet);
-            return "redirect:/vets/vetList";
+            return "redirect:/vets.html";
         }
     }
 }
